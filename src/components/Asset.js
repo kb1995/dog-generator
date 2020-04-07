cd887abb84f1435549daebfb282ccd48e35ea8c1
kb1995 <kristiyan.r.bogdanov@gmail.com>
2020-04-07 12:49:10 +0100

    Deploy

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,43 +1,73 @@
-import React from 'react';
+import React from "react";
 
-const imageTypes = ["jpg", "png", "jpeg", "gif"]
-const videoTypes = ["webm", "mp4"]
+const imageTypes = ["jpg", "png", "jpeg", "gif"];
+const videoTypes = ["webm", "mp4"];
 
 class Asset extends React.Component {
   static defaultProps = {
-    objectCover: true
-  }
+    objectCover: true,
+  };
 
   state = {
     loaded: false,
-    noLoadState: this.props.noLoadState || false
-  }
+    noLoadState: this.props.noLoadState || false,
+  };
 
-  componentDidUpdate(){
-    if(this.state.noLoadState ||this.state.loaded){
-      this.setState({ loaded: false })  
+  componentDidUpdate() {
+    if (this.state.noLoadState || this.state.loaded) {
+      this.setState({ loaded: false });
     }
   }
 
-  shouldComponentUpdate(nextProps, nextState) { 
+  shouldComponentUpdate(nextProps, nextState) {
     // a hack to reset the state after you load a new image. What's a more efficient way of doing it?
-    if (this.state.noLoadState || this.state.loaded) { 
+    if (this.state.noLoadState || this.state.loaded) {
       return false;
     }
     return true;
   }
-  
+
   handleAssetLoaded = () => {
-    this.setState({ loaded: true })
-  }
+    this.setState({ loaded: true });
+  };
 
   render() {
     return (
       <>
-        <p className={this.state.noLoadState ||this.state.loaded ? "hidden" : "block"}>loading...</p>
-        <div className={`${this.state.noLoadState ||this.state.loaded ? "block" : "hidden"} w-full h-full`}>
-          {imageTypes.includes(this.props.data.mediaType) && <img className={`${this.props.objectCover ? 'object-cover' : 'object-contain'} w-full h-full`} src={this.props.data.url} alt="" onLoad={() => this.handleAssetLoaded()} />}
-          {videoTypes.includes(this.props.data.mediaType) && <video className={`${this.props.objectCover ? 'object-cover' : 'object-contain'} w-full h-full`} src={this.props.data.url} controls autoPlay muted onLoadedData={() => this.handleAssetLoaded()} />}
+        <p
+          className={
+            this.state.noLoadState || this.state.loaded ? "hidden" : "block"
+          }
+        >
+          loading...
+        </p>
+        <div
+          className={`${
+            this.state.noLoadState || this.state.loaded ? "block" : "hidden"
+          } w-full h-full`}
+        >
+          {imageTypes.includes(this.props.data.mediaType) && (
+            <img
+              className={`${
+                this.props.objectCover ? "object-cover" : "object-contain"
+              } w-full h-full`}
+              src={this.props.data.url}
+              alt=""
+              onLoad={() => this.handleAssetLoaded()}
+            />
+          )}
+          {videoTypes.includes(this.props.data.mediaType) && (
+            <video
+              className={`${
+                this.props.objectCover ? "object-cover" : "object-contain"
+              } w-full h-full`}
+              src={this.props.data.url}
+              controls
+              autoPlay
+              muted
+              onLoadedData={() => this.handleAssetLoaded()}
+            />
+          )}
         </div>
       </>
     );
